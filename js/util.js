daf0d24d001ea573272f7c0321126fd0a3fa224b
Balaji Chockalingam <bchockalingam@mediaocean.com>
2018-01-30 20:40:12 -0500

    Added a javascript file to define a function that checks for page loaded or not.

---
+++ js/util.js
@@ -0,0 +1,32 @@
+var timeInterval = 5000,
+    myTimer;
+
+function callWhenReadyToGo(callback) {
+
+    var maxCheck = 5,
+        checkCounter = 0;
+
+    var checkIfLoadingIsDone = function () {
+        if (checkCounter === maxCheck) {
+            //something wrong with the loading, so just quit
+            console.warn('Exceeded maximum wait time');
+            clearInterval(myTimer);
+            return;
+        }
+
+
+        var loadingDiv = document.getElementById('loadingdiv');
+        if (loadingDiv) {
+            console.log('loading is completed');
+            clearInterval(myTimer);
+            callback();
+        }
+        else {
+            checkCounter++;
+        }
+
+    };
+
+    myTimer = setInterval(checkIfLoadingIsDone, timeInterval);
+
+}
